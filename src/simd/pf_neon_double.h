6b15770639ab3fe24b837943763aef37a306b40f
Yuriy Chernyshov <thegeorg@yandex-team.com>
2026-09-23 12:22:50 +0200

    Handle MSVC / clang-cl specific define
    This backports patch from chromium:
    https://chromium.googlesource.com/chromium/src/+/refs/heads/main/third_party/pffft/patches/05-fix-arch-detection.diff
    
    (We do not need x86 support, but we keep applying _M_ARM64 patch locally).

--- src/simd/pf_neon_double.h
+++ src/simd/pf_neon_double.h
@@ -40,7 +40,7 @@
   NEON 64bit support macros — native AArch64 implementation.
   Packs 4 doubles into a pair of float64x2_t (2x 128-bit NEON registers).
 */
-#if !defined(SIMD_SZ) && !defined(PFFFT_SIMD_DISABLE) && defined(PFFFT_ENABLE_NEON) && (defined(__aarch64__) || defined(__arm64__))
+#if !defined(SIMD_SZ) && !defined(PFFFT_SIMD_DISABLE) && defined(PFFFT_ENABLE_NEON) && (defined(__aarch64__) || defined(__arm64__) || defined(_M_ARM64))
 
 #pragma message (__FILE__ ": NEON double macros are defined" )
 
